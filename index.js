e70e06baf7d35835073df5e3a0e3b7f5322b56ec
Dylan Connor <DylanJConnor4@gmail.com>
2022-10-30 20:32:59 -0400

    set up cron

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+'use strict';
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
 const fs = require("fs");
 const axios = require('axios');
 const https = require('https');
@@ -11,6 +16,7 @@ const client = require('twilio')(accountSid, authToken);
 const phoneNumFrom = process.env.PHONE_FROM ? process.env.PHONE_FROM : credentials.PHONE_FROM;
 const phoneNumTo = process.env.PHONE_TO ? process.env.PHONE_TO : credentials.PHONE_TO;
 
+app.use(cors());
 
 function sendMessage(msg = 'Hello there!') {
   client.messages
@@ -27,26 +33,39 @@ const agent = new https.Agent({
   rejectUnauthorized: false
 });
 
-const config = {
-  method: 'GET',
-  url: 'https://zenquotes.io/api/random/',
-  // httpsAgent: agent
-};
+
+  // homepage
+app.get('/random-quote', (req, res) => {
+  if (res.statusCode === 200){
+
+  const config = {
+    method: 'GET',
+    url: 'https://zenquotes.io/api/random/',
+    // httpsAgent: agent
+  };
+  axios(config)
+    .then((response) => {
+      if (response.status === 200) {
+        console.log(response.data)
+        sendMessage(response.data[0].q + '\n--' + response.data[0].a);
+      } else {
+        res.sendStatus(404)
+      }
+    })
+    .catch((error) => {
+      if (error.response && error.response.data) {
+        console.log('ERROR:', error.response.data)
+      } else {
+        console.log('ERROR:', error)
+      }
+    });
+
+  } else {
+    res.sendStatus(404)
+  }
+});
 
 
-axios(config)
-  .then((response) => {
-    if (response.status === 200) {
-      console.log(response.data)
-      sendMessage(response.data.content);
-    } else {
-      res.sendStatus(404)
-    }
-  })
-  .catch((error) => {
-    if (error.response && error.response.data) {
-      console.log('ERROR:', error.response.data)
-    } else {
-      console.log('ERROR:', error)
-    }
-  });
+
+// connect to server
+app.listen(port, () => console.log(`Listening on port ${port}!`));
